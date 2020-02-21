86894169b67e1d24e558deab951e8aa6a79915cd
tyebkhan <30511534+tyebkhan@users.noreply.github.com>
2020-02-20 21:08:59 -0500

    Update Shop.java

--- Shop.java
+++ Shop.java
@@ -1,8 +1,7 @@
 import java.util.*;
 public class Shop{
-    static Player shopper = new Player();//creates a player that can be used to in the constructor
-    static Weapon weapon;//initializes a weapon to be sold
-    //constructor which prints out the player's options
+    static Player shopper = new Player();
+    static Weapon weapon;
     public Shop(Player p, Weapon w){
         shopper = p;
         weapon = w;
@@ -15,7 +14,7 @@ public class Shop{
         System.out.println("4. View Inventory");
         System.out.println("5. Return to Orbit");
     }
-    //method by which the player purchases items
+
     public static void shopping(Player p){
         System.out.println("What are you buying?");
         //gets user input between 1 and 5 
@@ -53,8 +52,7 @@ public class Shop{
             MenuMain.menuMainAction(p);
         }
     }
-    //sells a weapon to the player
-    //first checks if the player has enough credits to buy it then adds it to their inventory then equips it
+
     public static void sellWeapon(Player p, Weapon w){
         if(p.money>= w.wPrice){
             p.money = p.money-w.wPrice;//reduces the player's currency by the price of the weapon
@@ -68,12 +66,11 @@ public class Shop{
             Shop.shopping(p);
         }
     }
-    //sells medicine to a player
+
     public static void sellMedicine(Player p){
-        //checks if the player has enough credits to purchase the medicine
         if(p.money>=50){
             p.money = p.money-10;//reduces the player's currency by 10
-            p.MedicineList.get(0).uses = p.MedicineList.get(0).uses + 5;//adds medicine by increasing the player's medicine uses
+            p.MedicineList.get(0).uses = p.MedicineList.get(0).uses + 5;
             System.out.println(p.Name + " Purchased 5 Medicine Refill");
             Shop.shopping(p);
             //if the player doesn't have enough credits, they are taken back to the shop
@@ -82,9 +79,8 @@ public class Shop{
             Shop.shopping(p);
         }
     }
-    //sells ammo to the player
+
     public static void sellAmmo(Player p){
-        //checks if the player has enough credits to purchase ammo 
         if(p.money>=10){
             p.money = p.money - 10;//reduces the player's currency by 10
             p.WeaponList.get(0).ammo = p.WeaponList.get(0).ammo + 20;//adds the ammo the player's equipped weapon
@@ -94,7 +90,6 @@ public class Shop{
             System.out.println(p.Name + " Puchased more ammo"); 
             Shop.shopping(p);
         }else{
-            //if the player doesn't have enough credits, they are taken back to the shop
             System.out.println(p.Name + "does not have enough credits to purchase Ammo");
             Shop.shopping(p);
         }
