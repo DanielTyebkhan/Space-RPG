e8ba5254462a9c4727f474b103709e211be6310c
tyebkhan <30511534+tyebkhan@users.noreply.github.com>
2020-02-20 21:04:04 -0500

    Update Alien.java

--- Alien.java
+++ Alien.java
@@ -5,7 +5,6 @@ public class Alien{
     double armor;//stores the alien's armor
     double heal;//stores the alien's heal value
     double deathCredits;//stores the amount of gold the player will receive for killing an alien
-    //constructor which sets the above variables
     public Alien(double idamage, double ihp, double iarmor, double iheal, double ideathCredits){
         damage = idamage;
         maxHp = ihp;
@@ -14,16 +13,16 @@ public class Alien{
         heal = iheal;
         deathCredits = ideathCredits;
     }
-    //method which returns the alien's damage when it attacks
+
     double alienAttack(){
         return damage;
     }
-    //method where aliens hp is reduced accounting for the player's damage and the alien's armor
+
     public void alienDamaged(double i){
         i = i-(armor*.25);
         hp = hp-i;
     }
-    //method which heals the alien
+
     public void alienHeal(){
         hp = hp+heal;
         if(hp>maxHp){
