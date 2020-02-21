9bb5d8ea3791e4264d0655c96c1c421d7e748724
tyebkhan <30511534+tyebkhan@users.noreply.github.com>
2020-02-20 21:05:39 -0500

    Update Battle.java

--- Battle.java
+++ Battle.java
@@ -5,13 +5,11 @@ public class Battle{
     int alienTurn;
     int run;
     public Battle(Player p, Alien a){
-        //sets it so the player gets the first action
         playerTurn = 1;
         alienTurn = 2;
         run = 2;
-        //loop that alternates between the player and alien until one dies
+        
         while(p.hp>0 && a.hp>0){
-            //while loop for the player's turn. Starts with displaying the player's options
             while(playerTurn ==1 && run ==2){    
                 System.out.println("It is " + p.Name + "'s turn. Please select an Action");
                 System.out.println("1. Attack");
@@ -34,15 +32,15 @@ public class Battle{
                     }
                     action = input.nextInt();
                 }
-                //how the player attacks
+
                 if(action == 1){
                     if(p.playerAttack() > 0){
                         System.out.println(p.Name + " attacked");
-                        a.alienDamaged(p.playerAttack());//uses the alienDamaged and playerAttack methods to damage the alien
-                        playerTurn = 2;//makes it not the player's turn
-                        alienTurn = 1;//makes it the alien's turn
+                        a.alienDamaged(p.playerAttack());
+                        playerTurn = 2;
+                        alienTurn = 1;
                     }
-                    //if the alien's hp is less than 0 after being attacked, it is set to 0 because negative hp is impossible
+
                     if(a.hp<0){
                         a.hp = 0;
                         playerTurn = 2;
@@ -50,15 +48,13 @@ public class Battle{
                     }
                     System.out.println("alien has " + a.hp + "/" + a.maxHp + "hp");
                 }
-                //how the player heals
+
                 if(action == 2){
-                    //checks to make sure the player does not already have full hp 
                     if(p.hp == p.hpMax){
                         System.out.println(p.Name + " already has full hp");
                     }else{
                         //uses the player heal method to add hp
                         p.hp = p.hp+p.playerHeal();
-                        //if the players hp exceeds its maximum hp, it is set to the maximum hp
                         if(p.hp>p.hpMax){
                             p.hp = p.hpMax;
                         }
@@ -67,9 +63,8 @@ public class Battle{
                         System.out.println(p.Name + "'s hp is " + p.hp);
                     }
                 }
-                //how the player runs
+
                 if(action == 3){
-                    //player must be above 25% hp to run otherwise they must select a different action
                     if(p.hp/p.hpMax>.25){
                         runHp = p.hp;//sets a variable to store the player's hp to reset it after the player runs. the hps are set to 0 to break the while loop (Now I know it may have been possible to do this using a break command which I was not aware of while making this
                         p.hp = 0;
@@ -80,17 +75,16 @@ public class Battle{
                         System.out.println(p.Name + " is too weak and cannot run away");
                     }
                 }
-                //allows the player to change weapons
+
                 if(action == 4){
                     int index = 1;//creates an index variable for the player to reference
                     System.out.println("Please enter the weapon you would like to equip");
-                    //enhanced for loop to print the weapons' names and ammo
                     for(Weapon w: p.WeaponList){
                         System.out.print(index + ". " );
                         System.out.println(w.wName + " " + w.ammo + " ammo");
                         index = index + 1;
                     }
-                    //gets the user's input and ensures it is valid
+
                     Scanner equip = new Scanner(System.in);
                     while(!equip.hasNextInt()){
                         System.out.println("Please enter a valid number");
@@ -111,7 +105,7 @@ public class Battle{
                     System.out.println(p.Name + " equipped " + p.WeaponList.get(0).wName);
                 }
             }
-            //while loop for the alien's turn
+
             while(alienTurn == 1 && run == 2){
                 if(a.hp == a.maxHp){//makes it so the alien will always attack if it is at full hp
                     System.out.println("The alien attacks");
@@ -144,7 +138,7 @@ public class Battle{
                     }
                 }
             }
-            //below are the termination possibilities
+
             if(a.hp == 0 && run == 2){//if the alien runs out of hp, the player wins
                 System.out.println(p.Name + " defeated the alien");
                 p.money = a.deathCredits + p.money;//gives the player the alien's deathCredits
