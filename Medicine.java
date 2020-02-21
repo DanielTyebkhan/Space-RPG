90437e7c6cb4a8f2e593e970f59eba31d88e09fa
tyebkhan <30511534+tyebkhan@users.noreply.github.com>
2020-02-20 21:06:40 -0500

    Update Medicine.java

--- Medicine.java
+++ Medicine.java
@@ -1,9 +1,9 @@
 public class Medicine{
-    double uses;//instance variable to store the number of uses the medicine has left
-    double heal;//instance variable for how much hp the medicine gives
+    double uses;//number of uses the medicine has left
+    double heal;//how much hp the medicine gives
     double mPrice;//stores the price (I did not actually end up using this function)
     String mName;//stores the name of the medicine
-    //the following is a contstructor which sets all of the above variables
+
     public Medicine(double iheal, double iuses, String imName, double imPrice){
         uses = iuses;
         heal = iheal;
