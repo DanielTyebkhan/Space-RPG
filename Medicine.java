bd5b3c29cca4187b46dc4b4ef05282b2ca4126e7
tyebkhan <30511534+tyebkhan@users.noreply.github.com>
2020-03-15 10:46:04 -0400

    added javadocs to Medicine

--- Medicine.java
+++ Medicine.java
@@ -1,9 +1,19 @@
+/**
+ * @author Daniel Tyebkhan
+ */
 public class Medicine{
     private double uses;
     private double heal;
     private double price;
     private String name;
 
+    /**
+     * Constructor
+     * @param heal The amount the medicine heals for
+     * @param uses The initial number of uses for the medicine
+     * @param name The name of the medicine
+     * @param price The price of the medicine
+     */
     public Medicine(double heal, double uses, String name, double price){
         this.heal = heal;
         this.uses = uses;
@@ -11,14 +21,26 @@ public class Medicine{
         this.price = price;
     }
 
+    /**
+     * Gets the price of the medicine
+     * @return The medicine's price
+     */
     public double getPrice(){
         return price;
     }
 
+    /**
+     * Gets the name of the medicine
+     * @return The medicine's name
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     * Uses the medicine and removes one use
+     * @return The value to heal for if there are uses available, else 0
+     */
     public double use(){
         if(!hasUses()){
             System.out.println("That medicine is empty");
@@ -28,6 +50,10 @@ public class Medicine{
         return heal;
     }
 
+    /**
+     * Checks if the medicine has uses remaining
+     * @return true if the medicine has uses, else false
+     */
     private boolean hasUses(){
         return uses>0;
     }
