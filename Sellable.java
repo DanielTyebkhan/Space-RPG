6d4839829feec7ab7e8ad2eda018340253d1f3a5
tyebkhan <30511534+tyebkhan@users.noreply.github.com>
2020-03-15 16:32:08 -0400

    added docstrings to Sellable

--- Sellable.java
+++ Sellable.java
@@ -1,20 +1,40 @@
+/**
+ * @author Daniel Tyebkhan
+ */
 public abstract class Sellable {
     private double price;
     private String name;
 
+    /**
+     * Constructor
+     * @param name The Sellable's name
+     * @param price The Sellable's price
+     */
     public Sellable(String name, double price){
         this.name = name;
         this.price = price;
     }
 
+    /**
+     * Gets the Sellable's price
+     * @return The price of the Sellable
+     */
     public double getPrice(){
         return price;
     }
 
+    /**
+     * Gets the Sellable's name
+     * @return The name of the Sellable
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     * Transforms the Sellable into a string
+     * @return The Sellable's name and price
+     */
     @Override
     public String toString(){
         return name + " price: " + price + " credits";
